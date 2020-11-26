e35d4b990521e301cc543cce080667fef3707963
Unknown <chalvantzis12@outlook.com>
2020-11-26 14:11:18 +0100

    undo test

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,11 @@ app.use((req: Request, res: Response, next) => {
 
 app.use(
     (
-        
+        error: { status: any; message: any },
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
         res.status(error.status || 500);
         res.json({
             error: {
