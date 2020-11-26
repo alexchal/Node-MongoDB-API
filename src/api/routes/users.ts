28baacb10742073b6569202fc8724cd73b326dd5
Unknown <chalvantzis12@outlook.com>
2020-11-26 20:59:59 +0100

    rename files

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as UserController from "../controllers/userController";
+import * as UserController from "../controllers/UserController";
 
 const router = Router();
 
