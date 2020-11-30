38f1b2fa32bbdfc7e50ae24a3259d18d590c62fc
Alexandros <chalvantzis12@outlook.com>
2020-11-30 19:53:49 +0100

    dynamic promises

--- src/api/routes/users/users.test.ts
+++ src/api/routes/users/users.test.ts
@@ -6,30 +6,43 @@ import { User, UserType } from "../../models/user";
 
 const request = supertest(app);
 
-describe("Users Route", () => {
+describe("Users Routeee", () => {
     setupDB.setupDB("endpoint-testing");
 
     it("it should fetch users", async (done) => {
-        const user = new User({
-            _id: new mongoose.Types.ObjectId(),
-            email: "fakeemai@gmail.com",
-            password: "testtest"
-        });
+        const users = [
+            new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: "fakeemai@gmail.com",
+                password: "testtest"
+            }),
+            new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: "fakeemai@gmail.com",
+                password: "testtest"
+            })
+        ];
 
-        await user.save();
+        const mongoDBPromises = [];
+
+        for (let i = 0; i < users.length; i++) {
+            mongoDBPromises.push(await users[i].save());
+        }
+
+        await Promise.all(mongoDBPromises);
 
         const res = await request.get("/api/users");
 
-        console.log("user", res.body);
         expect(res.status).toBe(200);
-        expect(res.body.count).toBe(1);
-        res.body.users.map((user: UserType) => {
-            expect(user).toMatchObject({
-                _id: user._id,
-                email: user.email,
+        expect(res.body.count).toBe(users.length);
+
+        res.body.users.map((user: UserType, i: number) => {
+            expect(user).toEqual({
+                _id: users[i]._id.toJSON(),
+                email: users[i].email,
                 request: {
                     type: "GET",
-                    url: `http://localhost:8080/api/products/${user._id}`
+                    url: `http://localhost:8080/api/products/${users[i]._id}`
                 }
             });
         });
